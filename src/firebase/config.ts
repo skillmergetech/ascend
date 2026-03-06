2d3d8e9e672eb9cddf0ba7959275fff74234f293
Skill Merge <skillmerge.tech@gmail.com>
2026-03-06 16:09:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1326987727-b182b",
+  "appId": "1:120367505483:web:43b7d941f051fde2e46dce",
+  "apiKey": "AIzaSyDGGqtqdtwR6yIdWqEalXm4RejIWzkicRY",
+  "authDomain": "studio-1326987727-b182b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "120367505483"
+};
